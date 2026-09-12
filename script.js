5aed4d1ee8a8dfd3ae1d1e879967b64b4c9aea47
cdrnext <www.chanukad@gmail.com>
2026-09-12 12:09:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // Play music automatically after 3 seconds
   setTimeout(() => {
     playMusic();
-  }, 3000);
+  }, 2000);
 
   // Target Date: Nov 12, 2026 10:04:00 AM (Poruwa Ceremony Nekatha)
   const weddingDate = new Date('November 12, 2026 10:04:00').getTime();
